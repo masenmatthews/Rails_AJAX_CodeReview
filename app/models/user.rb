658272313e540c75b68229f0d1e89f2be2aefceb
Masen Matthews <masenmatthews@gmail.com>
2018-05-04 15:36:00 -0700

    reset database, add validations for password length - 3/6 objectives complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   has_many :orders
 
+  validates :password, :presence => true, length: { in: 6..18 }, :on => :create
+  validates :password, :confirmation => true, length: { in: 6..18 }, :on => :update
+
   def previous_orders
     self.orders.where(status: 2).includes(order_items: :product)
   end
