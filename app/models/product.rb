dcf095b1400b4ee21d39ab7dc9e5e5084bf2756e
Masen Matthews <masenmatthews@gmail.com>
2018-05-04 14:25:17 -0700

    continued troubleshooting

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   has_many :order_items
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/pictures/:style/missing.png"
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "app/assets/images/missing.jpeg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
