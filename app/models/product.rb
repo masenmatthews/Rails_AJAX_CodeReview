0a06108db8ba377e6097cc5f1ffbb7fa46843f9c
Masen Matthews <masenmatthews@gmail.com>
2018-05-04 15:56:55 -0700

    add product validation - name, description, and price must be present. 4/6 objectives complete

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   has_many :order_items
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/pictures/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  validates :name, :description, :price, :presence => true
 end
