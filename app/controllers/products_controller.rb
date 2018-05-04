9077bed34ded7d32f8cdc00e3524b1f86332c2e5
Masen Matthews <masenmatthews@gmail.com>
2018-05-04 14:56:13 -0700

    remove authentication requirements for new and create products methods. add product form now functional, images still not showing up

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_filter :authorize, except: [:index, :show]
+  before_filter :authorize, except: [:index, :show, :new, :create]
 
   def index
     @products = Product.all
