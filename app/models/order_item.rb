de0b94b1ea3a141f30fe1986f84709bb3e8cc3e1
Masen Matthews <masenmatthews@gmail.com>
2018-05-04 12:20:07 -0700

    add validation so that a negative number of items can not be added to the cart - 2/6 objectives complete

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,6 +2,6 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
 
-  # validates :order_items, numericality: { greater_than_or_equal_to_1: true }
+  validates :order_items, numericality: { greater_than_or_equal_to_1: true }
 
 end
