60e20b206d20ec25fe81698bfd8739951ae370cd
Masen Matthews <masenmatthews@gmail.com>
2018-05-04 12:17:02 -0700

    add flash alerts for sign up and sign out - 1/6 objectives completed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      flash[:notice] = "Sign in successful!"
+      flash[:alert] = "Sign in successful!"
       redirect_to '/'
     else
       redirect_to '/sign_in'
@@ -16,6 +16,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
+    flash[:alert] = "Sign out successful!"
     redirect_to '/sign_in'
   end
 
